ca2135f3a8768a8834e3e8c9d8a8c1de0ed03c7e
Vishal Bhalerao <vishalbhalerao.bhalerao@gmail.com>
2021-10-09 10:00:23 +0530

    connecting redux to react using react-redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import Application from './Application';
-
 import './index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Application />
-  </React.StrictMode>,
+  /**
+   * Provider is API of react-redux library which connects store to our application
+   */
+  <Provider store={store} >
+    <React.StrictMode>
+      <Application />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
